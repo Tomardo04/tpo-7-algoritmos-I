328c8a778ce03607ab7aff17f2de957628beb1c3
Tomardo04 <tomasbrunobasilico1@gmail.com>
2024-09-09 21:19:50 -0300

    Updated juego.py, added main.py, menu.py and palabras.py

--- juego.py
+++ juego.py
@@ -1,10 +1,14 @@
 import random
+import string as str
+import re
 
 # - Variables globales
 
 INTENTOS = 6
-palabras = ["Variable", "Función", "Algoritmo","Depuración", "Compilador", "Objeto"]
+palabras = ["variable", "funcion", "algoritmo","depuracion", "compilador", "objeto", "computadora", "juego", "string", "random", "mouse", "teclado", "python", "javascript", "chrome", "facultad", "clase", "aula", "memoria", "procesador", "almacenamiento", "impresora", ""]
 bandera = True
+ganaste = False
+perdiste = False
 dibujos = [
     '''
        +---+
@@ -71,6 +75,14 @@ dibujos = [
     '''
 ]
 
+# - Funciones globales
+
+def VerificarPalabra(palabra):
+    if re.match("^[a-zA-Z]+$", palabra):
+        return True
+    else:
+        return False
+
 # - Funciones del juego
 
 def Juego():
@@ -79,83 +91,131 @@ def Juego():
     palabraSecreta = len(palabra) * "_"
     juego = True
     fallidos = 0
+    utilizadas = []
     while juego == True:
         print("\n" * 10)
         print(dibujos[fallidos])
         print("Intentos fallidos:",fallidos,"\n")
-        print(palabraSecreta, "\n")
-        print(palabra)
+        print(palabraSecreta.capitalize(), "\n")
+        if len(utilizadas) > 0:
+            print("Letras utilizadas:",utilizadas)
+        
+        if palabraSecreta == palabra:
+            Ganaste(palabra)
+            juego = False
+            break
         if fallidos == INTENTOS:
             Perdiste(palabra)
+            juego = False
+            break
         
-        seEncuentra = False
         banderaEntrada = True
         while banderaEntrada == True:
             entrada = input("Ingrese una letra o adivine la palabra: ")
+            entrada = entrada.lower()
             if len(entrada) == 1:
-                for i in range(len(palabra)-1):
-                    if entrada == palabra[i]:
-                        palabraSecreta[i] = palabra[i]
-                        seEncuentra = True
-                if seEncuentra == False:
-                    fallidos += 1
+                if entrada not in utilizadas:
+                    utilizadas.append(entrada)
+                    seEncuentra = False
+                    for i in range(len(palabra)):
+                        if entrada == palabra[i]:
+                            palabraSecreta = list(palabraSecreta)
+                            palabra = list(palabra)
+                            palabraSecreta[i] = palabra[i]
+                            palabraSecreta = "".join(palabraSecreta)
+                            palabra = "".join(palabra)
+                            seEncuentra = True
+                    if seEncuentra == False:
+                        fallidos += 1
                 banderaEntrada = False
             elif len(entrada) > 2:
-                if entrada == palabra:
-                    juego == False
+                if VerificarPalabra(entrada) == True:
+                    if entrada == palabra:
+                        Ganaste(palabra)
+                        juego = False
+                    else:
+                        fallidos = INTENTOS
+                    banderaEntrada = False
                 else:
-                    fallidos = INTENTOS
-                banderaEntrada = False
+                    print("Intente con una palabra válida")
             else:
-                print("Intentalo de vuelta.")
+                print("Intentalo de vuelta")
             
 
-
-
 def Perdiste(palabra):
-    band = True
-    while band == True:
-        print("Perdiste! La palabra era:",palabra,"\nQueres intentar de nuevo? (Y / N)")
-        x = input()
-        if x == "y":
-            band == False
-            Juego()
-        elif x == "n":
-            band == False
-        else:
-            print("\n" * 10)
-            print("Por favor, ingrese una de las 2 opciones que se pidió.")
+    print("Perdiste! La palabra correcta era:", palabra.capitalize(),"\nPresiona Enter para continuar")
+    x = input()
 
+def Ganaste(palabra):
+    print("Felicitaciones! La palabra era:", palabra.capitalize(),"\nPresiona Enter para continuar")
+    x = input()
     
-# - Funciones de ingresar una nueva palabra
+# - Funciones de gestion de palabras
 
 def IngresarPalabra():
+    band = True
+    while band == True:
+        x = input("Ingrese una palabra de 3 caracteres o más que quiera agregar (Presione Enter para salir): ")
+        if len(x) == 0:
+            band = False
+        elif len(x) > 2:
+            x = x.lower()
+            if VerificarPalabra(x) == True and x not in palabras:
+                palabras.append(x)
+                print("Se ha ingresado una nueva palabra:", x.capitalize())
+            else:
+                print("Ingrese una palabra válida")
+        else:
+            print("Intentalo de vuelta")
+
+def EliminarPalabra():
+    band = True
+    while band == True:
+        print(palabras)
+        x = input("Ingrese una palabra que quiera eliminar (Presione Enter para salir): ")
+        if len(x) == 0:
+            band = False
+        elif len(x) > 2:
+            x = x.lower()
+            if VerificarPalabra(x) == True and x in palabras:
+                palabras.remove(x)
+                print("Se ha eliminado una palabra:", x.capitalize())
+            else:
+                print("Ingrese una palabra válida")
+        else:
+            print("Intentalo de vuelta")
+
+# - Funciones de gestion de jugadores
+
+def GuardarPartida():
     print("No funca")
 
-#def VerificarPalabra()
-
-# - Funciones de cargar lista de jugadores
-
-def CargarListaJugadores():
+def ListaDeJugadores():
     print("No funca")
 
 # - Menu principal
 
 while bandera == True:
     print("\n" * 10)
-    print("  ___   _   _ ___________  _____   ___ ______ _____ \n / _ \ | | | |  _  | ___ \/  __ \ / _ \|  _  \  _  | \n/ /_\ \| |_| | | | | |_/ /| /  \// /_\ \ | | | | | | \n|  _  ||  _  | | | |    / | |    |  _  | | | | | | |\n| | | || | | \ \_/ / |\ \ | \__/\| | | | |/ /\ \_/ /\n\_| |_/\_| |_/\___/\_| \_| \____/\_| |_/___/  \___/\n----------------------------------------------------")
+    print("  ___   _   _ ___________  _____   ___ ______ _____ \n / _ \ | | | |  _  | ___ \/  __ \ / _ \|  _  \  _  | \n/ /_\ \| |_| | | | | |_/ /| /  \// /_\ \ | | | | | | \n|  _  ||  _  | | | |    / | |    |  _  | | | | | | |\n| | | || | | \ \_/ / |\ \ | \__/\| | | | |/ /\ \_/ /\n\_| |_/\_| |_/\___/\_| \_| \____/\_| |_/___/  \___/\n\nBy Tomás B., alumno UADE\n----------------------------------------------------")
     print("1 - Empezar nuevo juego")
     print("2 - Agregar palabra")
-    print("3 - Lista histórica de jugadores")
-    print("4 - Salir\n----------------------------------------------------")
+    print("3 - Eliminar palabra")
+    print("4 - Guardar partida")
+    print("5 - Lista histórica de jugadores")
+    print("6 - Salir\n----------------------------------------------------")
     x = int(input("Ingrese el número de la opción que quiere elegir: "))
     if x == 1:
         Juego()
     elif x == 2:
         IngresarPalabra()
     elif x == 3:
-        CargarListaJugadores()
+        EliminarPalabra()
     elif x == 4:
+        GuardarPartida()
+    elif x == 5:
+        ListaDeJugadores()
+    elif x == 6:
         bandera = False
         print("Saliste del juego")
     else:
